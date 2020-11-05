6fa13d7b18112595994fa67f674f616e97cab48b
pfilimonenkov <binf1611@gmail.com>
2020-11-05 18:45:55 +0300

    fix

--- mnbd.py
+++ mnbd.py
@@ -20,8 +20,16 @@ supported_fs = (
 )
 
 
-def mount(nbd_device, mount_point, mode, host, port):
+def unmount(nbd_device, mount_point):
+    parts = get_partition(nbd_device)
+    log.info(parts)
+    for part in parts:
+        device, path = get_device_path_of_partition(nbd_device, mount_point, part)
+        check_output(['umount', '-l', path])
+        check_output(['nbd-client', '-d', device])
 
+
+def mount(nbd_device, mount_point, mode, host, port):
     dir_path = Path(mount_point)
     if dir_path.exists() and dir_path.is_dir():
         shutil.rmtree(mount_point)
@@ -29,55 +37,59 @@ def mount(nbd_device, mount_point, mode, host, port):
 
     log.info('run nbd-client for %s...' % nbd_device)
     check_output([
-        'sudo',
         'nbd-client', '-p', '-t', '60', '-b', '512', host, port,
         nbd_device
     ])
 
-    partitions = get_partition(nbd_device)
-    log.info(partitions)
-    partition = None
-    for part in partitions:
-        partition = partitions[part]
-        partition['part'] = part
-        break
-
     # create dev mapper
     check_output(['kpartx', '-a', '-s', '-v', nbd_device])
 
+    parts = get_partition(nbd_device)
+    log.info(parts)
+
+    partitions = []
+
+    for part in parts:
+        partition = parts[part]
+        partition['part'] = part
+        device, path = get_device_path_of_partition(nbd_device, mount_point, part)
+
+        if partition['filesystem'] == 'ntfs':
+            # get cluster size
+            volume_info = get_ntfs_volume_info(device, force=True)
+            log.info("volume info: %s", volume_info)
+            cluster_size = int(volume_info['bytes_per_cluster'])
+            log.info('get cluster size for %s: %s', device, cluster_size)
+            partition['cluster_size'] = cluster_size
+
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+
+        partition['device'] = device
+        partition['path'] = path
+        partition['mount_point'] = mount_point
+        partition['nbd_device'] = nbd_device
+
+        log.info("partition: %s", partition)
+
+        check_output([
+            'mount', '-t', partition['filesystem'], '-o', mode,
+            device, path
+        ])
+
+        partitions.append(partition)
+
+    return partitions
+
+
+def get_device_path_of_partition(nbd_device, mount_point, part):
     nbd = nbd_device.split('/')[2]
-
-    device = '/dev/mapper/%sp%s' % (nbd, partition['part'])
-
-    if partition['filesystem'] == 'ntfs':
-        # get cluster size
-        volume_info = get_ntfs_volume_info(device, force=True)
-        cluster_size = int(volume_info['bytes_per_cluster'])
-        print('get cluster size for %s: %s' % (
-            device, cluster_size
-        ))
-        partition['cluster_size'] = cluster_size
-
-    path = os.path.join(mount_point, partition['part'])
-
-    if not os.path.exists(path):
-        os.makedirs(path, exist_ok=True)
-
-    partition['device'] = device
-    partition['path'] = path
-    partition['mount_point'] = mount_point
-    partition['nbd_device'] = nbd_device
-
-    check_output([
-        'sudo',
-        'mount', '-t', partition['filesystem'], '-o', mode,
-        device, path
-    ])
-    return partition
+    device = '/dev/mapper/%sp%s' % (nbd, part)
+    path = os.path.join(mount_point, part)
+    return device, path
 
 
 def get_partition(path):
-
     try:
         device = parted.Device(path)
         disk = parted.Disk(device)
@@ -119,4 +131,9 @@ def check_output(cmd):
 
 
 if __name__ == '__main__':
-    mount(*(sys.argv[1:]))
+    if sys.argv[1] == "-m":
+        mount(*(sys.argv[2:]))
+    elif sys.argv[1] == "-u":
+        unmount(*(sys.argv[2:]))
+    else:
+        raise Exception("unexpected key: %s", sys.argv[1])
