fcc59790d951571c4a75f26eb3a2480ac22dd757
pfilimonenkov <binf1611@gmail.com>
2020-11-05 19:50:04 +0300

    fix

--- mnbd.py
+++ mnbd.py
@@ -11,21 +11,17 @@ log = logging.getLogger(__name__)
 
 
 def unmount(nbd_device, mount_point):
-    parts = get_partition(nbd_device)
-    log.info(parts)
-    for part in parts:
-        _, path = get_device_path_of_partition(nbd_device, mount_point, part)
-        check_output(['umount', '-l', path])
-        check_output(['nbd-client', '-d', nbd_device])
+    check_output(['umount', '-l', mount_point])
+    check_output(['nbd-client', '-d', nbd_device])
 
 
 def mount(nbd_device, mount_point, mode, host, port):
+
     dir_path = Path(mount_point)
     if dir_path.exists() and dir_path.is_dir():
         shutil.rmtree(mount_point)
     dir_path.mkdir(parents=True, exist_ok=False)
 
-    log.info('run nbd-client for %s...' % nbd_device)
     check_output([
         'nbd-client', '-p', '-t', '60', '-b', '512', host, port,
         nbd_device
@@ -34,41 +30,14 @@ def mount(nbd_device, mount_point, mode, host, port):
     # create dev mapper
     check_output(['kpartx', '-a', '-s', '-v', nbd_device])
 
-    parts = get_partition(nbd_device)
-
-    partitions = []
-
-    for part in parts:
-        partition = parts[part]
-        partition['part'] = part
-        device, path = get_device_path_of_partition(nbd_device, mount_point, part)
-
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
-
-        partition['device'] = device
-        partition['path'] = path
-        partition['mount_point'] = mount_point
-        partition['nbd_device'] = nbd_device
-
-        log.info("partition: %s", partition)
-
-        check_output([
-            'mount', '-t', partition['filesystem'], '-o', mode,
-            device, path
-        ])
-
-        partitions.append(partition)
-        log.info(partition)
-
-    return partitions
+    check_output([
+        'mount', '-t', "ntfs", '-o', "rw",
+        get_device_mapper_path(nbd_device), mount_point
+    ])
 
 
-def get_device_path_of_partition(nbd_device, mount_point, part):
-    nbd = nbd_device.split('/')[2]
-    device = '/dev/mapper/%sp%s' % (nbd, part)
-    path = os.path.join(mount_point, part)
-    return device, path
+def get_device_mapper_path(nbd_device):
+    return '/dev/mapper/%sp1' % nbd_device.split('/')[2]
 
 
 def get_partition(path):
