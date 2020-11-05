8dd06ef354ce603f96e6b43d7276da71c3e79185
pfilimonenkov <binf1611@gmail.com>
2020-11-05 20:16:23 +0300

    fix

--- mnbd.py
+++ mnbd.py
@@ -1,4 +1,3 @@
-import parted
 import logging
 import subprocess
 import sys
@@ -12,23 +11,25 @@ log = logging.getLogger(__name__)
 def unmount(nbd_device, mount_point):
     check_output(['umount', '-l', mount_point])
     shutil.rmtree(mount_point)
+    # delete partition mappings
+    check_output(['kpartx', '-d', nbd_device])
     check_output(['nbd-client', '-d', nbd_device])
 
 
 def mount(nbd_device, mount_point, host, port):
 
-    dir_path = Path(mount_point)
-    if dir_path.exists() and dir_path.is_dir():
+    mount_point_dir_path = Path(mount_point)
+    if mount_point_dir_path.exists() and mount_point_dir_path.is_dir():
         shutil.rmtree(mount_point)
-    dir_path.mkdir(parents=True, exist_ok=False)
+    mount_point_dir_path.mkdir(parents=True, exist_ok=False)
 
     check_output([
         'nbd-client', '-p', '-t', '60', '-b', '512', host, port,
         nbd_device
     ])
 
-    # create dev mapper
-    check_output(['kpartx', '-a', '-s', '-v', nbd_device])
+    # create partition mappings
+    check_output(['kpartx', '-a', '-s', nbd_device])
 
     check_output([
         'mount', '-t', "ntfs-3g", '-o', "rw",
@@ -38,25 +39,6 @@ def mount(nbd_device, mount_point, host, port):
 
 def get_device_mapper_path(nbd_device):
     return '/dev/mapper/%sp1' % nbd_device.split('/')[2]
-
-
-def get_partition(path):
-    try:
-        device = parted.Device(path)
-        disk = parted.Disk(device)
-    except parted.IOException as e:
-        raise Exception('Failed to get parted info for %s' % path) from e
-    except parted.disk._ped.DiskLabelException as e:
-        raise Exception('failed to parse disk for %s' % path) from e
-
-    mapping = dict()
-    for partition in disk.partitions:
-        if partition.fileSystem is None:
-            raise Exception("Unknown file system: %s" % partition)
-        attr = dict()
-        attr['filesystem'] = partition.fileSystem.type
-        mapping[str(partition.number)] = attr
-    return mapping
 
 
 def check_output(cmd):
